b5cd3ab93ccbdf0fca4e910e03d06faa39c619fe
Nicolas Brassard <nico@nitriques.com>
2012-10-11 14:18:37 -0400

    Added an adhoc test

--- grunt.js
+++ grunt.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
 		pkg: '<json:package.json>',
 		meta: {
 			banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-			'<%= grunt.template.today("yyyy-mgit com-dd") %>\n' +
+			'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
 			'<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
 			'* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
 			' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
